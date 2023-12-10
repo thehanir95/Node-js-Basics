8f93c54df13e1c6b84d2ea7bd7031b4c3f06da0e
thehanir95 <thehanicis95@gmail.com>
2023-12-10 09:45:43 +0530

    use same module multi times in files

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const addFun = require("./add")
 const sum = addFun(1, 3)
 console.log('sum', sum);
 
+const sum2 = addFun(6, 10)
+console.log('sum2', sum2);
+
 // we can type (node index) in terminal to run the node File
 
