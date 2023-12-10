326217858e62b2845095280f2fa50e06ec13c803
thehanir95 <thehanicis95@gmail.com>
2023-12-10 09:43:56 +0530

    using module exports from transfer modules in other files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-require("./add.js")
+const addFun = require("./add")
 
-console.log('run by node');
+
+const sum = addFun(1, 3)
+console.log('sum', sum);
 
 // we can type (node index) in terminal to run the node File
 
