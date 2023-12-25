d3cf5f4a59359d5cbe9b0d11ca87d210f7248560
thehanir95 <thehanicis95@gmail.com>
2023-12-25 11:06:32 +0530

    calling multi functions in same js file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const add = require('./math')
+const math = require('./math')
 
-console.log('add', add(3, 4));
+console.log('add', math.add(3, 4));
+console.log('sub', math.substract(3, 4));
--- math.js
+++ math.js
@@ -1,5 +1,14 @@
 
-module.exports = (a, b) => {
+const add = (a, b) => {
     return a + b
+}
+
+const substract = (a, b) =>{
+    return a - b
+}
+
+module.exports = {
+    add,
+    substract
 }
 
